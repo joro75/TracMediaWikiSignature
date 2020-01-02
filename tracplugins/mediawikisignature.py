d569ba88d73e08d455f870ecde1433aea4cc3d99
John de Rooij <john.de.rooij@gmail.com>
2020-01-02 22:19:24 +0100

    Initial working proof of concept of signature replacer

--- tracplugins/mediawikisignature.py
+++ tracplugins/mediawikisignature.py
@@ -6,3 +6,29 @@
 # This software is licensed as described in the file LICENSE, which
 # you should have received as part of this distribution.
 
+from trac.core import Component, implements
+from trac.util import get_reporter_id
+from trac.wiki.api import IWikiPageManipulator
+
+class MediaWikiSignatureManipulator(Component):
+    _description = "Replaces the MediaWiki signature with the author name."
+
+    implements(IWikiPageManipulator)
+
+    # IWikiPageManipulator
+
+    def prepare_wiki_page(self, req, page, fields):
+        pass
+
+    def validate_wiki_page(self, req, page):
+        text = page.text
+        if len(text):
+            fullname = req.session.get('name') or ''
+            username = get_reporter_id(req)
+            signature = username
+            if len(fullname):
+                signature = fullname + ' (%s)' % username
+
+            page.text = text.replace('~~~~', signature)
+
+        return []
