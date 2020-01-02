2221b7cb8a0ecb9a2a2649a17e920c30faed6cae
John de Rooij <john.de.rooij@gmail.com>
2020-01-02 21:25:33 +0100

    Created intial setuptools package

---
+++ tracplugins/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: iso-8859-1 -*-
+#
+# Copyright (C) 2020 John de Rooij
+# All rights reserved.
+#
+# This software is licensed as described in the file LICENSE, which
+# you should have received as part of this distribution.
+
+
+__all__ = ["mediawikisignature"]
