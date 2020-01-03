a5fc1bf1986a9bc379347a38a31e7c13b969fdcf
John de Rooij <john.de.rooij@gmail.com>
2020-01-03 23:26:16 +0100

    Implemented the replacement of the signature, also added a first version of the 'user' TracLink

--- tracplugins/mediawikisignature.py
+++ tracplugins/mediawikisignature.py
@@ -6,14 +6,18 @@
 # This software is licensed as described in the file LICENSE, which
 # you should have received as part of this distribution.
 
+from datetime import datetime
+
 from trac.core import Component, implements
 from trac.util import get_reporter_id
-from trac.wiki.api import IWikiPageManipulator
+from trac.util.datefmt import format_datetime, localtz, user_time
+from trac.wiki.api import IWikiPageManipulator, IWikiSyntaxProvider
+from trac.util.html import tag
 
 class MediaWikiSignatureManipulator(Component):
     _description = "Replaces the MediaWiki signature with the author name."
 
-    implements(IWikiPageManipulator)
+    implements(IWikiPageManipulator, IWikiSyntaxProvider)
 
     # IWikiPageManipulator
 
@@ -21,14 +25,40 @@ class MediaWikiSignatureManipulator(Component):
         pass
 
     def validate_wiki_page(self, req, page):
-        text = page.text
-        if len(text):
-            fullname = req.session.get('name') or ''
-            username = get_reporter_id(req)
-            signature = username
-            if len(fullname):
-                signature = fullname + ' (%s)' % username
+        startpos = page.text.find('~~~')
+        if startpos >= 0:
+            # Only search and replace in the part that we found
+            # a 3-tilde signature
+            prefix = page.text[:startpos]
+            replacetext = page.text[startpos:]
 
-            page.text = text.replace('~~~~', signature)
+            fullname = req.session.get('name')
+            username = get_reporter_id(req)
+            signature = ''.join(['[[user:', username, '|', fullname or username, ']]'])
+
+            tzinfo = getattr(req, 'tz', None)
+            now = datetime.now(tzinfo or localtz)
+            today = format_datetime(now, 'iso8601', tzinfo)
+            today_user = user_time(req, format_datetime, now, tzinfo=tzinfo)
+            timestamp = ''.join(['[[timeline:', today, '|', today_user, ']]'])
+
+            # Replace the tilde signature starting with the longest
+            replacetext = replacetext.replace('~~~~~', '-- ' + timestamp)
+            replacetext = replacetext.replace('~~~~', '-- ' + signature + ' ' + timestamp)
+            replacetext = replacetext.replace('~~~', '-- ' + signature)
+
+            page.text = prefix + replacetext
 
         return []
+
+    # IWikiSyntaxProvider methods
+
+    def get_wiki_syntax(self):
+        return []
+
+    def get_link_resolvers(self):
+        def link_resolver(formatter, ns, target, label):
+            username = target
+            return tag.a(label, title='Link to user: ' + username, href='https://www.google.com')
+
+        yield 'user', link_resolver
