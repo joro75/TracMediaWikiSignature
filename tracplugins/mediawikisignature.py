dbc8ea90e8b16f2045c91086cf33aa213cf0fce2
John de Rooij <john.de.rooij@gmail.com>
2020-01-04 16:39:08 +0100

    Implemented the intial working version of the Signature macro

--- tracplugins/mediawikisignature.py
+++ tracplugins/mediawikisignature.py
@@ -8,11 +8,13 @@
 
 from datetime import datetime
 
-from trac.core import Component, implements
+from trac.core import Component, implements, TracError
 from trac.util import get_reporter_id
-from trac.util.datefmt import format_datetime, localtz, user_time
+from trac.util.datefmt import format_datetime, localtz, user_time, parse_date
 from trac.wiki.api import IWikiPageManipulator, IWikiSyntaxProvider
 from trac.util.html import tag
+from trac.wiki.formatter import format_to_oneliner
+from trac.wiki.macros import WikiMacroBase
 
 class MediaWikiSignatureManipulator(Component):
     _description = "Replaces the MediaWiki signature with the author name."
@@ -34,18 +36,15 @@ class MediaWikiSignatureManipulator(Component):
 
             fullname = req.session.get('name')
             username = get_reporter_id(req)
-            signature = ''.join(['[[user:', username, '|', fullname or username, ']]'])
 
             tzinfo = getattr(req, 'tz', None)
             now = datetime.now(tzinfo or localtz)
             today = format_datetime(now, 'iso8601', tzinfo)
-            today_user = user_time(req, format_datetime, now, tzinfo=tzinfo)
-            timestamp = ''.join(['[[timeline:', today, '|', today_user, ']]'])
 
             # Replace the tilde signature starting with the longest
-            replacetext = replacetext.replace('~~~~~', '-- ' + timestamp)
-            replacetext = replacetext.replace('~~~~', '-- ' + signature + ' ' + timestamp)
-            replacetext = replacetext.replace('~~~', '-- ' + signature)
+            replacetext = replacetext.replace('~~~~~', ''.join(['-- [[Signature(,', today, ')]]']))
+            replacetext = replacetext.replace('~~~~', ''.join(['-- [[Signature(', username, ', ', today, ', ', fullname or '', ')]]']))
+            replacetext = replacetext.replace('~~~', ''.join(['-- [[Signature(', username, ', , ', fullname or '', ')]]']))
 
             page.text = prefix + replacetext
 
@@ -59,6 +58,62 @@ class MediaWikiSignatureManipulator(Component):
     def get_link_resolvers(self):
         def link_resolver(formatter, ns, target, label):
             username = target
-            return tag.a(label, title='Link to user: ' + username, href='https://www.google.com')
+            return tag.a(label, title='Link to user: ' + username, href='https://www.google.com', class_='trac-author-user')
 
         yield 'user', link_resolver
+
+class SignatureMacro(WikiMacroBase):
+    _description = (
+    """Expands the passed username, date and full-username to a MediaWiki like signature""")
+
+    def expand_macro(self, formatter, name, content, args):
+        if content:
+            args = content.split(',')
+
+            username = None
+            timestamp = None
+            fullusername = None
+            if len(args) > 0:
+                username = args[0]
+                if not username:
+                    username = username.strip()
+            if len(args) > 1:
+                timestamp = args[1]
+                if not timestamp:
+                    timestamp = timestamp.strip()
+            if len(args) > 2:
+                # Need to handle the situation that a , is present in the fullname
+                fullusername = args[2]
+                if not fullusername:
+                    fullusername = fullusername.strip()
+
+            if not username or len(username) == 0:
+                username = None
+            if not timestamp or len(timestamp) == 0:
+                timestamp = None
+            if not fullusername or len(fullusername) == 0:
+                fullusername = None
+
+            if not fullusername:
+                fullusername = username
+
+            signature = fullusername
+            if username:
+                signature = ''.join(['[[user:', username, '|', fullusername or username, ']]'])
+
+            timeline = ''
+            if timestamp:
+                tzinfo = getattr(formatter.context.req, 'tz', None)
+                try:
+                    dateobject = parse_date(timestamp)
+                    today_user = user_time(formatter.context.req, format_datetime, dateobject, tzinfo=tzinfo)
+                    timeline = ''.join(['[[timeline:', timestamp, '|', today_user, ']]'])
+                except TracError:
+                    pass
+
+            result = ''.join([signature or '', ' ', timeline or '']).strip()
+            # Convert Wiki markup to HTML
+            return format_to_oneliner(self.env, formatter.context, result)
+        else:
+            # No content is parsed
+            return ''
